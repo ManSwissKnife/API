848a36d101392d4ba80b7ae90fb13be9e4b4b04c
ManSwissKnife <62439500+ManSwissKnife@users.noreply.github.com>
2021-09-26 15:46:31 +0300

    Add files via upload

---
+++ API/API/Models/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
